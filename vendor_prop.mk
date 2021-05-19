6dfbba1709ca96f5e0a54221ad8ae56b0ccd1d2e
Stoner <jaycopter@foxmail.com>
2021-05-19 12:41:44 +0800

    msm8998-common: Force triple frame buffers
    Ref: LineageOS/android_device_google_wahoo/commit/5977232a
    
    Change-Id: Ifaa860d63c2696919e3ef212ddd7594b97c278fb

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -186,11 +186,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true
 
+# Force triple frame buffers
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
